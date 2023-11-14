dd60d79b025a21c08e10e703c1a356080b6d52b8
krishika123 <68084241+krishika123@users.noreply.github.com>
2023-11-13 22:15:31 -0500

    Update to main

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
-print('Hello world')
+# Taking input for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Multiplying the two numbers
+result = num1 * num2
+
+# Displaying the multiplication result
+print("The result of multiplying", num1, "and", num2, "is:", result)
