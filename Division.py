af5bf70dfbf74968e5742939b789df38e50ccb52
krishika123 <68084241+krishika123@users.noreply.github.com>
2023-11-15 22:29:39 +0000

    division added

---
+++ Division.py
@@ -0,0 +1,4 @@
+
+def divide_nums(a, b):
+    result = a / b
+    print("The result of dividing", a, "and", b, "is:", result)
--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 from Multiply import multiply_nums
+from Division import divide_nums
 # Taking input for two numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 multiply_nums(num1,num2)
+divide_nums(num1,num2)
